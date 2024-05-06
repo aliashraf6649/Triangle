dbc0b9527b573b852219c8ccda69ede5ba892ef8
aliashraf6649 <113792656+aliashraf6649@users.noreply.github.com>
2024-05-06 21:41:01 +0300

    .

--- insertion.py
+++ insertion.py
@@ -12,6 +12,7 @@ def Insertion_Sort(arr):
         while i >= 0 and arr[i] > key:
             arr[i+1] = arr[i]
             i = i-1
+        arr[i+1] = key
     return arr
 
 def is_triangular(nums):
